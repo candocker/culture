4538ee35149f839c916bd74084a2ed4fb4727c08
acanhost <acanstudio@canliang.wang>
2024-09-24 16:28:52 +0800

    develop culture dealed

--- app/Controllers/FigureController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ModuleCulture\Controllers;
-
-class FigureController extends AbstractController
-{
-}
--- app/Resources/ChapterCollection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ModuleCulture\Resources;
-
-class ChapterCollection extends AbstractCollection
-{
-}
